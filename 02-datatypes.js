b6beb089bd5c5ab3d1c175281ef732edee6fd2fe
Tarun Dhouni <tarundhouni2000@gmail.com>
2025-04-15 14:26:28 +0530

    first commit

---
+++ 02-datatypes.js
@@ -0,0 +1,20 @@
+"use Strict";//treat all javascript as newer version of javascript
+
+//alert(3+3) We are using NodeJs not browser
+
+//  One of the world most important thing in Code Redablility is most important
+
+
+//number => 2+2
+//string =>"Tarun"
+//boolean => true/false
+//null => Standalone write now
+//undefined => value is not assigned or defined
+//symbol ==> uniqness 
+
+// We use Special type of variable typeof()-> for knowing the type of valye
+// Example
+
+console.log(typeof "Tarun Dhouni");
+
+//We will study deeply about Object
